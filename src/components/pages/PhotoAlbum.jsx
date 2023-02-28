dbcc6eee78dcfef5a1d64e63cd2c7d1d39621044
Web-Dev-Robi <cordiscobrian@gmail.com>
2023-02-28 10:34:33 +0100

    fixed letter spacing

--- src/components/pages/PhotoAlbum.jsx
+++ src/components/pages/PhotoAlbum.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+// import Swiper core and required modules
+import { A11y, Navigation, Pagination, Scrollbar, Mousewheel } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { imagesLoad } from "../../images/images";
+// import bridge from "../../images/bridge.jpg";
 
 function PhotoAlbum() {
-  return (
-    <div>PhotoAlbum</div>
-  )
+	return (
+		<Swiper
+			modules={[Pagination, Mousewheel, Navigation, Scrollbar, A11y]}
+			onSwiper={(swiper) => (window.swiper = swiper)}
+			slidesPerView={3.5}
+			threshold={2}
+			spaceBetween={10}
+			navigation
+			scrollbar
+			mousewheel={{
+				forceToAxis: true,
+				sensitivity: 0.1,
+				releaseOnEdges: true,
+			}}
+			pagination={{ clickable: true }}>
+			<SwiperSlide>
+				<img
+					src={imagesLoad.sky}
+					alt=""
+				/>
+			</SwiperSlide>
+			<SwiperSlide>
+				<img
+					src={imagesLoad.sun}
+					alt=""
+				/>
+			</SwiperSlide>
+			<SwiperSlide>
+				<img
+					src={imagesLoad.clouds}
+					alt=""
+				/>
+			</SwiperSlide>
+			<SwiperSlide>
+				<img
+					src={imagesLoad.one}
+					alt=""
+				/>
+			</SwiperSlide>
+			<SwiperSlide>
+				<img
+					src={imagesLoad.nightbridge}
+					alt=""
+				/>
+			</SwiperSlide>
+		</Swiper>
+	);
 }
 
-export default PhotoAlbum
+export default PhotoAlbum;
---
+++ src/images/images.js
@@ -0,0 +1,10 @@
+export const imagesLoad = {
+    bridge: require('./bridge.jpg'),
+    sun: require('./sun.jpg'),
+    clouds: require('./clouds.jpg'),
+    sky: require('./sky.jpg'),
+    one: require('./one.jpg'),
+    nightbridge: require('./nightbridge.jpg'),
+   
+};
+
